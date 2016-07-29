f4305a5764e3826789fb05a31bb58b9f37b27b77
salarcon215 <steph@stephalarcon.org>
2016-07-29 16:58:10 +0200

    Get viable list of reboot candidates

--- mlab-ssh-outage.sh
+++ mlab-ssh-outage.sh
@@ -1,6 +1,12 @@
 #!/bin/bash 
 
-rm -r sshalt_outage*
+rm -r ssh_outage*
+
+timestamp="$(date +%F_%H-%M)"
+mkdir ssh_outage_${timestamp}
+ssh_outage=ssh_outage_${timestamp}
+sshalt_down_nodes="ssh_outage_${timestamp}/sshalt_down_nodes.txt"
+ssh_down_nodes="ssh_outage_${timestamp}/ssh_down_nodes.txt"
 
 # Configure a user's login to Nagios.  For more information on netrc files, see
 # the manpage for curl.  If a netrc file doesn't exist, then the user will be
@@ -14,56 +20,32 @@ else
         echo -e '\n'
 fi
 
-echo "Creating the file /tmp/mlab-nodes from Nagios sshalt plugin"
-curl -s $nagios_auth -o /tmp/mlab-nodes-sshalt --digest --netrc "http://nagios.measurementlab.net/baseList?show_state=1&service_name=sshalt&plugin_output=1"
-curl -s $nagios_auth -o /tmp/mlab-nodes-ssh --digest --netrc "http://nagios.measurementlab.net/baseList?show_state=1&service_name=ssh&plugin_output=1"
-
-full_host=1
-if [ "$1" == "-v" ]
-then
-	full_host=0
-	echo "Full_host value is " $full_host
-	shift
-fi
-
-timestamp="$(date +%F_%H-%M)"
-mkdir sshalt_outage_${timestamp}
-outage="sshalt_outage_${timestamp}/sshalt_down_nodes.txt"
-echo "Searching /tmp/mlab-nodes-sshalt for nodes in state 2"
-while read line
+# Grab all results from Nagios for sshalt and ssh
+for plugin in sshalt ssh
 do
-#    node=$(echo $line | sed "s/\(mlab[1-4]\.[a-z]\{3\}[0-9]\{2\}\).*/\1/")
-#    echo "Node is $node".
-	host=$(echo $line | awk '{print $1 }')
-	state=$(echo $line | awk '{ print $2 }')
-	hard=$(echo $line | awk '{ print $3 }')
+echo "Creating /tmp/mlab-nodes-$plugin from Nagios"
+curl -s $nagios_auth -o /tmp/mlab-nodes-$plugin --digest --netrc "http://nagios.measurementlab.net/baseList?show_state=1&service_name=$plugin&plugin_output=1"
 
-	if [ $state == 2 ] && [ $hard == 1 ]
-	then
-		echo "State of $host is $state and is in hard down state." >> $outage
-		echo $line >> $outage	
-		continue
-	fi
-#    echo $line | grep "CRITICAL" 1>/dev/null 2>&1
-#    if [ $? -ne 0 ]
-#    then
-#        continue
-#    fi
-#    ssh -n $node.measurement-lab.org "traceroute eb.measurementlab.net" > outage_${timestamp}/$node-ks.traceroute &
-#	echo $node >> $outage
-done < "/tmp/mlab-nodes-sshalt"
+# Look for nodes in state 2 in baseList.pl, which indicates a bad state, 
+# and state duration of 1, a "hard" state, meaning it's been that way for a while 
 
-echo "Searching /tmp/mlab-nodes-ssh for nodes in state 2"
-while read line
-do
-	host=$(echo $line | awk '{print $1 }')
-	state=$(echo $line | awk '{ print $2 }')
-	hard=$(echo $line | awk '{ print $3 }')
+	echo "Searching /tmp/mlab-nodes-${plugin} for nodes in hard state 2"
+	while read line
+	do
+		host=$(echo $line | awk '{print $1 }')
+		state=$(echo $line | awk '{ print $2 }')
+		hard=$(echo $line | awk '{ print $3 }')
 
-	if [ $state == 2 ] && [ $hard == 1 ]
-	then
-		echo "State of $host is $state and is in hard down state." >> $outage
-		echo $line >> $outage	
-		continue
-	fi
-done < "/tmp/mlab-nodes-ssh"
+		if [ $state == 2 ] && [ $hard == 1 ]
+		then
+			echo $host |grep -v ^s| awk -F. '{ print $1"."$2 }' >> $ssh_outage/${plugin}_down_nodes
+			echo $host |grep ^s | awk -F. '{ print $1"."$2 }' >> $ssh_outage/${plugin}_down_switches
+		fi
+		done < "/tmp/mlab-nodes-${plugin}"
+done
+rm $ssh_outage/sshalt_down_switches
+cd $ssh_outage
+comm --nocheck-order -12 *nodes* > reboot_candidates
+for line in `cat ssh_down_switches | awk -F. '{ print $2 }'`
+do grep -v $line reboot_candidates > reboot_me
+done
