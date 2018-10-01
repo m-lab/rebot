b7ed98728e4df38a0c296e3b83df2005f695c9a2
Ross Schulman <ross@rbs.io>
2018-10-01 11:29:54 -0400

    Read/store site history from temp file
    Adds capability to store a history of site availability in a temporary file and filter reboot list based on time since last reboot.

--- src/main.go
+++ src/main.go
@@ -9,16 +9,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 /// Struct to hold history of a given service's outages
-type Candidate {
-	Name string
-	LastDown Time
+type Candidate struct {
+	Name     string
+	LastDown time.Time
 }
 
-/// Structs that replicate the Prometheus query response
-/// JSON that we're getting. 'Value' is an interface
+/// Run and other following structs replicate the Prometheus query
+/// response JSON that we're getting. 'Value' is an interface
 /// because that JSON object is an array of two different
 /// types, which cannot be expressed in a type-safe language.
 type Run struct {
@@ -58,8 +59,8 @@ func getStats(username string, password string) []byte {
 		// If we can't access Prometheus, just exit
 		log.Fatal(err)
 	}
-	defer resp.Close()
 	bodyText, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
 	return bodyText
 }
 
@@ -93,11 +94,21 @@ func getCredentials() (string, string) {
 func main() {
 	// Call prometheus API for ssh806 service over 15m
 	// Sum should be 15. If < 15 query again to see if up now
-	candidate_history = make(map[string]Candidate)
+
+	// First, check to see if there's an existing candidate history file
+	var candidateHistory map[string]Candidate
+	file, err := ioutil.ReadFile("/tmp/candidateHistory.json")
+	if err != nil {
+		// There is no existing candidate history file...
+		candidateHistory = make(map[string]Candidate)
+	} else {
+		json.Unmarshal(file, &candidateHistory)
+	}
+
 	user, pass := getCredentials()
-	promJson := getStats(user, pass)
+	promJSON := getStats(user, pass)
 	var marshalRun Run
-	json.Unmarshal(promJson, &marshalRun)
+	json.Unmarshal(promJSON, &marshalRun)
 	var candidates []string
 	for _, site := range marshalRun.Data.Result {
 		if site.Value[1] != "15" {
@@ -105,7 +116,33 @@ func main() {
 		}
 	}
 	fmt.Println(candidates)
+	var realCandidates []string
 	for _, site := range candidates {
-
+		candidate, ok := candidateHistory[site]
+		if ok {
+			// This candidate has been down before.
+			// Check to see if the previous time was w/in the past 24 hours
+			if time.Now().Sub(candidate.LastDown) > 24*time.Hour {
+				// If previous incident was more than 24 hours ago,
+				// its still a candidate, so add it to the list
+				realCandidates = append(realCandidates, candidate.Name)
+			}
+			// Update the candidate with the current time and update the map
+			candidate.LastDown = time.Now()
+			candidateHistory[site] = candidate
+		} else {
+			// There's no candidate object in the map for this site
+			// so we have to create one and add it.
+			candidateHistory[site] = Candidate{
+				Name:     site,
+				LastDown: time.Now(),
+			}
+			realCandidates = append(realCandidates, site)
+		}
 	}
+	newCandidates, err := json.Marshal(candidateHistory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile("/tmp/candidateHistory.json", newCandidates, 0644)
 }
