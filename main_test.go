5e9976b213845e1d13a53bd2c46c36b040b2d850
Roberto D'Auria <roberto@measurementlab.net>
2018-11-30 15:19:28 +0000

    Run tests on different ports.

--- main_test.go
+++ main_test.go
@@ -188,6 +188,7 @@ func Test_main_oneshot(t *testing.T) {
 	defer restore()
 
 	ctx, cancel = context.WithCancel(context.Background())
+	listenAddr = ":9000"
 
 	main()
 
@@ -201,6 +202,7 @@ func Test_main_multi(t *testing.T) {
 	defer restore()
 
 	ctx, cancel = context.WithCancel(context.Background())
+	listenAddr = ":9001"
 
 	go func() {
 		time.Sleep(1 * time.Second)
