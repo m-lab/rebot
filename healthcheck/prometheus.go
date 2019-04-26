426dbcd3bc3268683c2aa1e543acd3f9ab6fdd12
Roberto D'Auria <roberto@measurementlab.net>
2019-04-26 16:51:12 +0200

    Change ssh806 to ssh on the "unless" clause, too.

--- healthcheck/prometheus.go
+++ healthcheck/prometheus.go
@@ -17,7 +17,7 @@ var (
 			unless on(machine) gmx_machine_maintenance == 1
 			unless on(site) gmx_site_maintenance == 1
 			unless on (machine) lame_duck_node == 1
-			unless on (machine) count_over_time(probe_success{service="ssh806", module="ssh_v4_online"}[%[1]dm]) < 14
+			unless on (machine) count_over_time(probe_success{service="ssh", module="ssh_v4_online"}[%[1]dm]) < 14
 			unless on (machine) rate(ndt_test_total[%[1]dm]) > 0`
 
 	// SwitchQuery is a prometheus query to determine what switches are
