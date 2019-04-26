0c29d27fbe1f49c70dc6a33921a19eab0fe0403e
Roberto D'Auria <roberto@measurementlab.net>
2019-04-26 16:16:28 +0200

    Fix healthcheck query to correctly monitor CoreOS nodes.
    This is needed as criteria for monitoring CoreOS nodes are different.
    
    Changes:
    - Check only service="ssh" (port 22)
    - "rate(ndt_test_total[15m]) > 0" is used instead of file creation rate

--- healthcheck/prometheus.go
+++ healthcheck/prometheus.go
@@ -12,16 +12,13 @@ import (
 )
 
 var (
-	NodeQuery = `(label_replace(sum_over_time(probe_success{service="ssh806", module="ssh_v4_online"}[%[1]dm]) == 0,
+	NodeQuery = `label_replace(sum_over_time(probe_success{service="ssh", module="ssh_v4_online"}[%[1]dm]) == 0,
 	"site", "$1", "machine", ".+?\\.(.+?)\\..+")
-	unless on (machine)
-	label_replace(sum_over_time(probe_success{service="ssh", module="ssh_v4_online"}[%[1]dm]) > 0,
-	"site", "$1", "machine", ".+?\\.(.+?)\\..+"))
 			unless on(machine) gmx_machine_maintenance == 1
 			unless on(site) gmx_site_maintenance == 1
 			unless on (machine) lame_duck_node == 1
 			unless on (machine) count_over_time(probe_success{service="ssh806", module="ssh_v4_online"}[%[1]dm]) < 14
-			unless on (machine) rate(inotify_extension_create_total{ext=".s2c_snaplog"}[%[1]dm]) > 0`
+			unless on (machine) rate(ndt_test_total[%[1]dm]) > 0`
 
 	// SwitchQuery is a prometheus query to determine what switches are
 	// offline.  To determine if a switch is offline, pings are generally
