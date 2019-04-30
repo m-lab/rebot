687d3f0eb0c248b63061ec566b6d54c472d7e352
Roberto D'Auria <roberto@measurementlab.net>
2019-04-30 14:18:44 +0200

    Add new lame-duck metric format to NodeQuery.

--- healthcheck/prometheus.go
+++ healthcheck/prometheus.go
@@ -12,6 +12,11 @@ import (
 )
 
 var (
+	// NodeQuery is a Prometheus query to determine what nodes aren't reachable
+	// on any of the currently used SSH ports (22 or 806). Additional filtering
+	// based on gmx maintenance, lame-duck mode, or the presence of recent NDT
+	// tests on the node is applied. This makes sure we never reboot a node
+	// unnecessarily or lose data.
 	NodeQuery = `label_replace(sum_over_time(probe_success{service="ssh", module="ssh_v4_online"}[%[1]dm]) == 0,
 	"site", "$1", "machine", ".+?\\.(.+?)\\..+")
 			unless on (machine)
@@ -20,6 +25,7 @@ var (
 			unless on(machine) gmx_machine_maintenance == 1
 			unless on(site) gmx_site_maintenance == 1
 			unless on (machine) lame_duck_node == 1
+			unless on (machine) kube_node_spec_taint{key="lame-duck"} == 1
 			unless on (machine) count_over_time(probe_success{service="ssh", module="ssh_v4_online"}[%[1]dm]) < 14
 			unless on (machine) rate(inotify_extension_create_total{ext=".s2c_snaplog"}[%[1]dm]) > 0
 			unless on (machine) increase(ndt_test_total[%[1]dm]) > 0`
